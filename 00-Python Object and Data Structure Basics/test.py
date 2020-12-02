7415fde1b1080d192ee27fced3b340a6ed36b769
HansSchuller <71433212+HansSchuller@users.noreply.github.com>
2020-12-02 07:43:42 +0100

    02.12.20_1

---
+++ 00-Python Object and Data Structure Basics/test.py
@@ -0,0 +1,24 @@
+
+print("hello \n world") # \n Zeilenumbruch
+print("hello \nworld") # \n Zeilenumbruch
+print("hello \tworld") # \n Tab
+
+# length
+
+print(len("Hans"))
+
+
+# Indexing
+
+name ="Hans"
+# Indexing
+print(name[3])
+# Reverse Indexing
+print(name[-1])
+
+# Slicing
+
+name = "Hans Schuller"
+print(name[2:])
+print(name[:3])
+print(name[0:4:1])
